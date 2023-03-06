1ab933eba43d0457aa0d1f1348052dc3629cf721
Agnisis Dutta <82666025+Agnisis@users.noreply.github.com>
2023-03-06 13:29:45 +0530

    initial

---
+++ server.js
@@ -0,0 +1,29 @@
+const express=require('express');
+const app=express();
+const ejs=require('ejs');
+const expresslayout=require('express-ejs-layouts');
+const PORT=process.env.PORT || 3000;
+const path =require('path');
+
+
+
+
+app.get('/',(req,res)=>{
+// res.snde("Hello from server");
+res.render('home');
+})
+
+
+
+
+//settemplate engine
+
+app.use(expresslayout);
+app.set('views',path.join(__dirname,'/resources/views'))
+app.set('view engine','ejs');
+app.listen(PORT,()=>{
+    console.log('====================================');
+    console.log(`listening on port  ${PORT}`);
+    console.log("its a fantastic server ");
+    console.log('====================================');
+})
