463e6fa5a7ee0826877c98c027691efac9bc9367
Agnisis Dutta <82666025+Agnisis@users.noreply.github.com>
2023-07-08 17:27:08 +0530

    initial

--- server.js
+++ server.js
@@ -4,14 +4,10 @@ const ejs=require('ejs');
 const expresslayout=require('express-ejs-layouts');
 const PORT=process.env.PORT || 3000;
 const path =require('path');
-
-
+const initRoutes = require('./routes/web');
 
 // Assets 
 app.use(express.static('public'));
-
-
-
 
 //settemplate engine
 
@@ -19,27 +15,27 @@ app.use(expresslayout);
 app.set('views',path.join(__dirname,'/resources/views'))
 app.set('view engine','ejs');
 
+// app.get('/',(req,res)=>{
+//     // res.snde("Hello from server");
+//     res.render('home');
+//     })
 
 
 
-app.get('/',(req,res)=>{
-    // res.snde("Hello from server");
-    res.render('home');
-    })
+
+// app.get('/cart',(req,res)=>{
+    // res.render('customers/cart')
+    // })
     
-    
-app.get('/cart',(req,res)=>{
-res.render('customers/cart')
-})
-
-app.get('/login',(req,res)=>{
-    res.render('auth/login')
-})
-
-app.get('/register',(req,res)=>{
-    res.render('auth/register')
-})
-
+    // app.get('/login',(req,res)=>{
+        //     res.render('auth/login')
+        // })
+        
+        // app.get('/register',(req,res)=>{
+            //     res.render('auth/register')
+            // })
+            
+            require('./routes/web')(app)
     
 app.listen(PORT,()=>{
     console.log('====================================');
